af008c0e86a0b628912f1b28422f8a80d85e0f8d
krisb454 <147145419+krisb454@users.noreply.github.com>
2024-10-03 09:00:48 +0530

    Update script.py

--- script.py
+++ script.py
@@ -34,13 +34,13 @@ Add me as admin in your group and start filtering :)
 /id  -  Shows ID information
 /info userid  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
-© @learningbots79
+© @darksquad14
 """
 
 AboutTxT = """
-⭕️ Official Channel : @learningbots79</b>
-⭕️ Developers : @TroJanzHEX 
-⭕️ Upgrader : @Mo_Tech_YT
+⭕️ Official Channel : @darksquad14</b>
+⭕️ Developers : @shikamaaru_naara
+⭕️ Upgrader : @shikamaaru_naara
 ⭕️ Language : Python3
 ⭕️ Library : <a href='https://docs.pyrogram.org/'>Pyrogram 2.0.102</a> 
 """
